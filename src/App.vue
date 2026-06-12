2fd58fd8d4ac6b4d50e4a65fbbae1912c7573e15
liyuxin599X <13753318927@163.com>
2026-06-12 11:24:04 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,17 @@
 <template>
   <div id="app">
-    <Header @page-change="handlePageChange" />
-    <main class="main-content">
-      <component :is="currentComponent" />
-    </main>
+    <h1>Hello Vue 3 + Element Plus</h1>
+    <el-button type="primary">点击测试</el-button>
   </div>
 </template>
 
 <script>
-import { ref, markRaw } from 'vue';
-import Header from './components/Header.vue';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import { ElButton } from 'element-plus';
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Home,
-    About
-  },
-  setup() {
-    const currentComponent = ref(markRaw(Home));
-
-    const handlePageChange = (page) => {
-      if (page === 'home') {
-        currentComponent.value = markRaw(Home);
-      } else if (page === 'about') {
-        currentComponent.value = markRaw(About);
-      }
-    };
-
-    return {
-      currentComponent,
-      handlePageChange
-    };
+    ElButton
   }
 };
 </script>
@@ -42,9 +19,11 @@ export default {
 <style>
 #app {
   min-height: 100vh;
-}
-
-.main-content {
-  padding-top: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
 }
 </style>
